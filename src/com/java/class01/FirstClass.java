93927706fcb6163d2a1c2e3e1250c458de02d7e6
OlegKinakh <leto893@gmail.com>
2022-04-22 19:04:58 -0500

    Changes in class01

--- src/com/java/class01/FirstClass.java
+++ src/com/java/class01/FirstClass.java
@@ -20,6 +20,7 @@ public static void main(String[] args) {
     System.out.println("     Hello     ");
     System.out.println("     World      ");
     System.out.println("     World      ");
+    System.out.println( " ");
         }
 
 
